e4eaaa3fd0533b8faa979687d5b7a2117d7ec4e1
DaNaRim <rangar.danarim@gmail.com>
2020-12-06 15:20:29 +0200

    add Repository annotation in RelationshipDao

--- src/main/java/com/findme/dao/RelationshipDao.java
+++ src/main/java/com/findme/dao/RelationshipDao.java
@@ -3,9 +3,11 @@ package com.findme.dao;
 import com.findme.exception.InternalServerException;
 import com.findme.model.Relationship;
 import com.findme.model.RelationshipStatus;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface RelationshipDao {
 
     Relationship save(Relationship relationship) throws InternalServerException;
